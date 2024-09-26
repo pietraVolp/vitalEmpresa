dea3fc612aed84480c721f80c00db6fe18667e63
PIETRA PAULA VOLPATO <23111825@dsn127m1241958.cfp127aluno.local>
2024-09-26 14:44:56 -0300

    ok

--- Back/banco.sql
+++ Back/banco.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE db_vital;
 USE db_vital;
 
-drop database db_vital;
+ drop database db_vital;
 
 -- Tabela de sexos
 CREATE TABLE tbl_sexo (
@@ -9,17 +9,20 @@ CREATE TABLE tbl_sexo (
     descricao VARCHAR(20) NOT NULL UNIQUE
 );
 
+select * from tbl_sexo where descricao = 'Feminino';
+
 select * from tbl_sexo where id_sexo = 1;
 
 -- Inserir sexos padrão
 INSERT INTO tbl_sexo (descricao) VALUES ('Masculino');
 INSERT INTO tbl_sexo (descricao) VALUES ('Feminino');
 
+
+
 -- Tabela de empresas
 CREATE TABLE tbl_empresa (
     id_empresa INT AUTO_INCREMENT PRIMARY KEY,
     nome_empresa VARCHAR(100) NOT NULL,
-    nome VARCHAR(100) NOT NULL,
     nome_proprietario VARCHAR(100) NOT NULL,
     senha VARCHAR(255) NOT NULL,      -- Certifique-se de hashear as senhas (ex: bcrypt)
     cnpj VARCHAR(18) NOT NULL UNIQUE,
@@ -28,13 +31,15 @@ CREATE TABLE tbl_empresa (
     telefone_clinica VARCHAR(30) NOT NULL
 );
 
-drop procedure sp_inserir_empresa_com_endereco;
+select * from tbl_empresa;
+
+select id_empresa from tbl_empresa where cnpj = 12345678910 and senha = 'Vini123';
+
 
 DELIMITER $$
 
 CREATE PROCEDURE sp_inserir_empresa_com_endereco(
 IN p_nome_empresa VARCHAR(255),
-    IN p_nome VARCHAR(255),
     IN p_nome_proprietario VARCHAR(255),
     IN p_email VARCHAR(320),
     IN p_senha VARCHAR(15),
@@ -52,8 +57,8 @@ BEGIN
     DECLARE last_user_id INT;
 
     -- Inserindo o usuário
-    INSERT INTO tbl_empresa (nome_empresa, nome, nome_proprietario, senha, cnpj, email, telefone, telefone_clinica)
-    VALUES (p_nome_empresa, p_nome, p_nome_proprietario, p_senha, p_cnpj, p_email, p_telefone, p_telefone_clinica);
+    INSERT INTO tbl_empresa (nome_empresa,  nome_proprietario, senha, cnpj, email, telefone, telefone_clinica)
+    VALUES (p_nome_empresa, p_nome_proprietario, p_senha, p_cnpj, p_email, p_telefone, p_telefone_clinica);
    
     -- Obtendo o ID do usuário recém-inserido
     SET last_user_id = LAST_INSERT_ID();
@@ -63,11 +68,12 @@ BEGIN
     VALUES (p_cep, p_logradouro, p_bairro, p_cidade, p_estado, last_user_id);
 END$$
 
+
+
 DELIMITER ;
 CALL sp_inserir_empresa_com_endereco(
 'VITAL+',
     'João Silva',
-    'João Silva Costa Souza',
     'joão@gmail.com',
     'joão123',
     '1234567891011',
@@ -85,7 +91,6 @@ CREATE VIEW vw_empresas_enderecos AS
 SELECT
     u.id_empresa,
     u.nome_empresa,
-    u.nome,
     u.nome_proprietario,
     u.email,
     u.senha,
@@ -131,19 +136,10 @@ CREATE TABLE tbl_usuarios (
     FOREIGN KEY (id_sexo) REFERENCES tbl_sexo(id_sexo)
 );
 
--- Tabela de Endereços
-CREATE TABLE tbl_enderecos (
-    id_endereco INT PRIMARY KEY AUTO_INCREMENT,
-    cep VARCHAR(20),
-    logradouro VARCHAR(255),
-    complemento VARCHAR(255),
-    cidade VARCHAR(150),
-estado VARCHAR(20),
-    numero VARCHAR(30),
-    id_usuario INT,
-    CONSTRAINT FK_ENDERECO_USUARIO
-    FOREIGN KEY (id_usuario) REFERENCES tbl_usuarios(id_usuario)
-);
+
+drop table tbl_enderecos;
+
+select * from tbl_enderecos where id_endereco = 2;
 
 -- Tabela de médicos
 CREATE TABLE tbl_medicos (
@@ -198,6 +194,8 @@ END$$
 
 DELIMITER ;
 
+drop procedure sp_inserir_medico_ultima_empresa;
+
 CALL sp_inserir_medico_ultima_empresa(
     'Dra. Ana Pereira',            -- Nome do médico
     'dra.ana@email.com',           -- Email do médico
@@ -216,7 +214,7 @@ SELECT
     m.crm,
     m.data_nascimento AS data_nascimento_medico,
     e.id_empresa,
-    e.nome AS nome_empresa,
+    e.nome_empresa,
     e.nome_proprietario,
     e.cnpj,
     e.email AS email_empresa,
@@ -227,13 +225,42 @@ FROM
     tbl_medicos m
 JOIN
     tbl_empresa e ON m.id_empresa = e.id_empresa;
-   
+    
     drop view vw_medico_empresa;
+    
+    CREATE VIEW vw_medico_empresa AS
+SELECT
+    m.id_medico,
+    m.nome AS nome_medico,
+    m.email AS email_medico,
+    m.telefone AS telefone_medico,
+    m.crm,
+    m.data_nascimento AS data_nascimento_medico,
+    e.id_empresa,
+    e.nome_empresa,
+    e.nome_proprietario,
+    e.cnpj,
+    e.email AS email_empresa,
+    e.telefone,
+    e.telefone_clinica,
+    es.nome -- mostra as especialidades
+FROM
+    tbl_medicos m
+JOIN
+    tbl_empresa e ON m.id_empresa = e.id_empresa
+JOIN
+    tbl_medico_especialidade me ON m.id_medico = me.id_medico
+JOIN
+    tbl_especialidades es ON me.id_especialidade = es.id_especialidade;
+
+    
+    
+   
+ 
    
     select * from vw_medico_empresa;
    
     desc tbl_empresa;
-
 
 
 DELIMITER $$
@@ -258,7 +285,7 @@ BEGIN
     -- Pegar o ID do médico recém-cadastrado
     SET last_medico_id = LAST_INSERT_ID();
 
-    -- Dividir a lista de especialidades (separadas por vírgula) e fazer a associação
+    -- Loop para dividir as especialidades separadas por vírgula e associá-las ao médico
     WHILE LENGTH(p_especialidades) > 0 DO
         -- Pegar a primeira especialidade da lista
         SET especialidade_nome = TRIM(SUBSTRING_INDEX(p_especialidades, ',', 1));
@@ -283,25 +310,39 @@ END$$
 
 DELIMITER ;
 
+
 CALL sp_inserir_medico_com_especialidades(
-    'Dr. vini',           -- Nome do médico
-    'dr.vinio@exemplo.com',         -- Email do médico
-    'senhaSegura123',              -- Senha do médico
-    '11998765432',                 -- Telefone do médico
-    'CRM12332773',                   -- CRM do médico
-    '1980-10-20',                  -- Data de nascimento do médico
-    'Cardiologista, Ginecologista'      -- Especialidades separadas por vírgula
+    'Dr. João Silva',
+    'joao.silva@hospital.com',
+    'senhaSegura123',
+    '(11) 99999-9999',
+    '126541-SP',
+    '1980-05-20',
+    'Dermatologista' -- Especialidades separadas por vírgula
 );
+
+select * from tbl_especialidades;
+
+
+
+
+
+
+
 
 SELECT
     m.nome AS nome_medico,
-    e.nome AS especialidade
+    e.nome AS especialidade,
+    emp.nome_empresa AS nome_empresa
 FROM
     tbl_medico_especialidade me
 JOIN
     tbl_medicos m ON me.id_medico = m.id_medico
 JOIN
-    tbl_especialidades e ON me.id_especialidade = e.id_especialidade;
+    tbl_especialidades e ON me.id_especialidade = e.id_especialidade
+JOIN
+    tbl_empresa emp ON m.id_empresa = emp.id_empresa;
+
 
 
 
@@ -314,18 +355,26 @@ CREATE TABLE tbl_especialidades (
     imagem_url VARCHAR(255) NOT NULL -- URL da imagem armazenada no Firebase
 );
 
+select * from tbl_especialidades;
+
+select * from tbl_especialidades where nome like "%Ort%";
+
+show tables;
+
 insert into tbl_especialidades(nome, descricao, imagem_url)values
 (
-"Ginocologista",
-"Um cardiologista é um médico especializado em cuidar do coração e vasos sanguíneos.",
+"Cardiologista",
+"Um cardiologista é um médico que trata doenças do coração e vasos sanguíneos.",
 "https://vitaclinica.com.br/wp-content/uploads/2019/08/Cardiologista-1-1200x800.jpg"
 );
 
+select * from tbl_medico_especialidade;
+
 -- Tabela intermediária médico-especialidade
 CREATE TABLE tbl_medico_especialidade (
+id_medico_especialidade INT PRIMARY KEY AUTO_INCREMENT,
     id_medico INT,
     id_especialidade INT,
-    PRIMARY KEY (id_medico, id_especialidade),
     FOREIGN KEY (id_medico) REFERENCES tbl_medicos(id_medico) ON DELETE CASCADE, -- Exclui especialidades associadas se médico for removido
     FOREIGN KEY (id_especialidade) REFERENCES tbl_especialidades(id_especialidade) ON DELETE CASCADE -- Exclui associações se especialidade for removida
 );
@@ -338,6 +387,50 @@ CREATE TABLE tbl_empresa_especialidade (
     FOREIGN KEY (id_empresa) REFERENCES tbl_empresa(id_empresa) ON DELETE CASCADE, -- Exclui especialidades associadas se empresa for removida
     FOREIGN KEY (id_especialidade) REFERENCES tbl_especialidades(id_especialidade) ON DELETE CASCADE -- Exclui associações se especialidade for removida
 );
+
+DELIMITER $$
+
+CREATE PROCEDURE sp_inserir_especialidade_com_empresa(
+    IN p_nome_especialidade VARCHAR(255),
+    IN p_descricao_especialidade TEXT,
+    IN p_imagem_url VARCHAR(255)
+)
+BEGIN
+    DECLARE last_empresa_id INT;
+
+    -- Inserir a especialidade na tabela tbl_especialidades
+    INSERT INTO tbl_especialidades (nome, descricao, imagem_url)
+    VALUES (p_nome_especialidade, p_descricao_especialidade, p_imagem_url);
+
+    -- Pegar o ID da última empresa cadastrada
+    SELECT MAX(id_empresa) INTO last_empresa_id FROM tbl_empresa;
+
+    -- Verificar se existe pelo menos uma empresa cadastrada
+    IF last_empresa_id IS NOT NULL THEN
+        -- Pegar o ID da última especialidade inserida
+        SET @last_especialidade_id = LAST_INSERT_ID();
+
+        -- Associar a especialidade à última empresa
+        INSERT INTO tbl_empresa_especialidade (id_empresa, id_especialidade)
+        VALUES (last_empresa_id, @last_especialidade_id);
+    ELSE
+        -- Se não houver empresas cadastradas, lança um erro
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Nenhuma empresa encontrada para associar a especialidade';
+    END IF;
+END$$
+
+DELIMITER ;
+
+CALL sp_inserir_especialidade_com_empresa(
+    'Ortopedista',
+    'Especialista em problemas ósseos e musculares.',
+    'https://link_da_imagem.com/ortopedista.jpg'
+);
+select * from tbl_empresa_especialidade;
+
+
+
 
 -- Tabela de avaliações
 CREATE TABLE tbl_avaliacoes (
@@ -362,45 +455,73 @@ CREATE TABLE tbl_videos (
     FOREIGN KEY (id_empresa) REFERENCES tbl_empresa(id_empresa) ON DELETE CASCADE -- Se empresa for excluída, vídeos são excluídos
 );
 
+DELIMITER $$
+
+CREATE PROCEDURE sp_inserir_video_ultima_empresa(
+    IN p_titulo VARCHAR(255),
+    IN p_descricao TEXT,
+    IN p_url VARCHAR(255)
+)
+BEGIN
+    DECLARE last_empresa_id INT;
+
+    -- Obtém o id da última empresa cadastrada
+    SELECT MAX(id_empresa) INTO last_empresa_id FROM tbl_empresa;
+
+    -- Verifica se existe ao menos uma empresa cadastrada
+    IF last_empresa_id IS NOT NULL THEN
+        -- Insere o vídeo associado à última empresa cadastrada
+        INSERT INTO tbl_videos (id_empresa, titulo, descricao, url)
+        VALUES (last_empresa_id, p_titulo, p_descricao, p_url);
+    ELSE
+        -- Se não houver nenhuma empresa cadastrada, gera um erro
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Nenhuma empresa encontrada para associar o vídeo';
+    END IF;
+END$$
+
+DELIMITER ;
+
+
+CALL sp_inserir_video_ultima_empresa(
+    'Exercícios Físicos',
+    'Este vídeo apresenta alguns exercícios físicos.',
+    'https://url_do_video.com/video.mp4'
+);
+
+
+CREATE VIEW vw_videos_empresas AS
+SELECT
+    v.id_video,
+    v.titulo AS titulo_video,
+    v.descricao AS descricao_video,
+    v.url AS url_video,
+    v.data_publicacao,
+    e.id_empresa,
+    e.nome_empresa,
+    e.cnpj,
+    e.email AS email_empresa,
+    e.telefone AS telefone_empresa,
+    e.telefone_clinica
+FROM
+    tbl_videos v
+JOIN
+    tbl_empresa e ON v.id_empresa = e.id_empresa;
+
+
+select * from vw_videos_empresas;
+
+
+
+
 -- Índices para melhorar a performance de buscas frequentes
 CREATE INDEX idx_email_usuario ON tbl_usuarios(email);
 CREATE INDEX idx_crm_medico ON tbl_medicos(crm);
 
-drop procedure sp_inserir_usuario_com_endereco;
 
-DELIMITER $$
 
-CREATE PROCEDURE sp_inserir_usuario_com_endereco(
-    IN p_nome VARCHAR(255),
-    IN p_email VARCHAR(320),
-    IN p_cpf VARCHAR(15),
-    IN p_id_sexo INT,
-    IN p_senha VARCHAR(255),
-    IN p_data_nascimento DATE,
-    IN p_cep VARCHAR(20),
-    IN p_logradouro VARCHAR(255),
-    IN p_complemento VARCHAR(255),
-    IN p_cidade VARCHAR(150),
-    IN p_estado VARCHAR(20),
-    IN p_numero VARCHAR(30)
-)
-BEGIN
-    -- A declaração de variáveis deve vir logo após o BEGIN
-    DECLARE last_user_id INT;
 
-    -- Inserindo o usuário
-    INSERT INTO tbl_usuarios (nome, email, cpf, id_sexo, senha, data_nascimento)
-    VALUES (p_nome, p_email, p_cpf, p_id_sexo, p_senha, p_data_nascimento);
-   
-    -- Obtendo o ID do usuário recém-inserido
-    SET last_user_id = LAST_INSERT_ID();
-   
-    -- Inserindo o endereço do usuário
-    INSERT INTO tbl_enderecos (cep, logradouro, complemento, cidade, estado, numero, id_usuario)
-    VALUES (p_cep, p_logradouro, p_complemento, p_cidade, p_estado, p_numero, last_user_id);
-END$$
 
-DELIMITER ;
 
 CREATE VIEW vw_usuarios_enderecos AS
 SELECT
@@ -410,40 +531,13 @@ SELECT
     u.senha,
     u.cpf,
     s.descricao AS sexo,
-    u.data_nascimento,
-    e.cep,
-    e.logradouro,
-    e.complemento,
-    e.cidade,
-    e.estado,
-    e.numero
+    u.data_nascimento
 FROM
     tbl_usuarios u
 JOIN
-    tbl_enderecos e ON u.id_usuario = e.id_usuario
-JOIN
     tbl_sexo s ON u.id_sexo = s.id_sexo;
    
-    drop view vw_usuarios_enderecos;
-
--- Teste da procedure
-CALL sp_inserir_usuario_com_endereco(
-    'João Silva',
-    'joao.silva@email.com',
-    '123.456.789-09',
-    1,  -- ID para 'Masculino'
-    'senhaSegura123',
-    '1985-08-15',
-    '01001-000',
-    'Rua Exemplo',
-    'Apt 101',
-    'São Paulo',
-    '123'
-);
-
--- Verificar dados
-SELECT * FROM vw_usuarios_enderecos;
-
+   
 
 DELIMITER $$
 
@@ -458,7 +552,6 @@ END$$
 
 DELIMITER ;
 
-drop trigger trg_delete_usuario_endereco;
 
 
 -- Deletar um usuário (e automaticamente seu endereço será deletado)
@@ -469,100 +562,67 @@ SELECT * FROM tbl_usuarios;
 
 
 
-CREATE VIEW vw_informacoes_usuario AS
+
+
+
+
+
+
+           
+
+
+
+
+
+
+
+
+
+CREATE VIEW vw_empresa_completa AS
 SELECT
-    u.id_usuario,
-    u.nome,
-    u.email,
-    u.cpf,
-    s.descricao AS sexo,
-    u.data_nascimento,
-    e.cep,
-    e.logradouro,
-    e.complemento,
-    e.cidade,
-    e.numero
+    e.id_empresa,
+    e.nome_empresa,
+    e.nome_proprietario,
+    e.cnpj,
+    e.email AS email_empresa,
+    e.telefone AS telefone_empresa,
+    e.telefone_clinica,
+    med.id_medico,
+    med.nome AS nome_medico,
+    med.email AS email_medico,
+    med.crm,
+    med.telefone AS telefone_medico,
+    med.data_nascimento AS data_nascimento_medico,
+    esp.nome AS especialidade,
+    esp.descricao AS descricao_especialidade,
+    esp.imagem_url AS url_imagem_especialidade,
+    ende.cep,
+    ende.logradouro,
+    ende.bairro,
+    ende.cidade,
+    ende.estado
 FROM
-    tbl_usuarios u
-JOIN
-    tbl_enderecos e ON u.id_usuario = e.id_usuario
-JOIN
-    tbl_sexo s ON u.id_sexo = s.id_sexo;
-   
-   
-   
+    tbl_empresa e
+LEFT JOIN
+    tbl_endereco_empresa ende ON e.id_empresa = ende.id_empresa
+LEFT JOIN
+    tbl_medicos med ON e.id_empresa = med.id_empresa
+LEFT JOIN
+    tbl_medico_especialidade me ON med.id_medico = me.id_medico
+LEFT JOIN
+    tbl_especialidades esp ON me.id_especialidade = esp.id_especialidade;
 
-
-drop procedure sp_login_usuario;
-
+select * from vw_empresa_completa;
 
 DELIMITER $$
 
-CREATE PROCEDURE sp_login_usuario (
-    IN p_email VARCHAR(320),
-    IN p_senha VARCHAR(255)
-)
+CREATE TRIGGER trg_delete_empresa_endereco
+BEFORE DELETE ON tbl_empresa
+FOR EACH ROW
 BEGIN
-    DECLARE v_senha_armazenada VARCHAR(255);
-   
-    -- Buscar a senha armazenada do usuário com base no e-mail
-    SELECT senha INTO v_senha_armazenada
-    FROM tbl_usuarios
-    WHERE email = p_email
-    LIMIT 1;
-   
-    -- Verificar se a senha fornecida corresponde à senha armazenada
-    IF v_senha_armazenada IS NOT NULL AND v_senha_armazenada = p_senha THEN
-        -- Se as senhas coincidirem, buscar as informações do usuário e do endereço
-        SELECT
-            u.id_usuario,
-            u.nome,
-            u.email,
-            u.cpf,
-            s.descricao AS sexo,
-            u.data_nascimento,
-            e.cep,
-            e.logradouro,
-            e.complemento,
-            e.cidade,
-            e.numero
-        FROM
-            tbl_usuarios u
-        JOIN
-            tbl_enderecos e ON u.id_usuario = e.id_usuario
-        JOIN
-            tbl_sexo s ON u.id_sexo = s.id_sexo
-        WHERE
-            u.email = p_email;
-    ELSE
-        -- Caso a senha não coincida
-        SELECT 'Senha incorreta' AS erro;
-    END IF;
+    -- Deletar o endereço associado à empresa
+    DELETE FROM tbl_endereco_empresa
+    WHERE id_empresa = OLD.id_empresa;
 END$$
 
 DELIMITER ;
-
-
-
-
-select * from tbl_usuarios;
-
-CALL sp_login_usuario(
-            'ribeiro@gmail.com',
-            'gustavo123'
-        );
-CALL sp_login_usuario(
-    'vinicius@gmail.com',  -- E-mail do usuário
-    '$2b$10$t0KkFgNLe2UQWvlDUVufcOseIWHVfTmEEzaGCNfWgYJECsodaad7i'         -- Senha fornecida pelo usuário
-);
-
-CALL sp_login_usuario(
-            'vinicius@gmail.com',
-            '$2b$10$rrgEAs9.bQ6KgfT/YMWL0e2sizSSo6YSOGIciVELG.J7Ikb/JQO4i'
-        );
-       
-        CALL sp_login_usuario(
-            'vinicius@gmail.com',
-            '$2b$04$KF0.X00O0BF33pOWYVa5GeGMCogRD6qoxt8YN4LuNxKOXGnf15qzC'
-        );
-select * from tbl_usuarios;
---
+++ Front/js/cadastromedicos.js
@@ -0,0 +1,57 @@
+'use strict'
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Obtém os elementos do DOM
+  const nome = document.getElementById('nome')
+  const email = document.getElementById('email')
+  const senha = document.getElementById('senha')
+  const crm = document.getElementById('crm')
+  const telefone = document.getElementById('telefone')
+  const dataNascimento = document.getElementById('dataNascimento')
+  const especialidades = document.getElementById('especialidades')
+  const cadastro = document.getElementById('cadastro')
+
+
+  // Verifica se o botão foi encontrado
+  if (cadastro) {
+    cadastro.addEventListener('click', async () => {
+      const userData = {
+        nome: nome.value,
+        email: email.value,
+        senha: senha.value,
+        crm: crm.value,
+        telefone: telefone.value,
+        data_nascimento: dataNascimento.value,
+        especialidades: especialidades.value,
+       
+      }
+
+      try {
+        const response = await fetch('http://localhost:8080/v1/vital/medico', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(userData),
+          redirect: 'manual'  // Evita seguir redirecionamentos automaticamente
+        })
+
+        // Verifica o código de status da resposta
+        if (response.ok) {
+          const result = await response.json()
+          alert('Usuário cadastrado com sucesso!')
+          console.log(result)
+          window.location.href = '/Front/html/medicos.html'
+        } else {
+          const result = await response.json()
+          alert(`Erro: ${result.message}`)
+        }
+      } catch (error) {
+        console.error('Erro ao cadastrar usuário:', error)
+        alert('Erro ao cadastrar usuário. Tente novamente.')
+      }
+    })
+  } else {
+    console.error('O botão de cadastro não foi encontrado no DOM')
+  }
+})
